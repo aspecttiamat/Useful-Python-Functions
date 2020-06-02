156c5aa7b8b765ce19ec489c4c11991c8fc631e2
aspecttiamat <65841504+aspecttiamat@users.noreply.github.com>
2020-06-02 14:08:23 -0500

    Update Change.py

--- Change.py
+++ Change.py
@@ -1,10 +1,33 @@
-def change(pay, cost):
+# This function was made to calculate change due, and return the greedy amount of bills needed.
+
+def change(amt):
     cur = [100, 50, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]
     chg = []
-    amt = pay - cost
+    a = amt
 
     for bill in cur:
-        while amt >= bill:
+        while a >= bill:
             chg.append(bill)
-            amt -= bill
+            a -= bill
     return chg, len(chg)
+
+
+# Proof of concept, doing the same thing, just recursively. 
+
+chg = []
+
+
+def recur_change(amt, x):
+    cur = [100, 50, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]
+    r = 0
+
+    if amt == 0:
+        return chg
+    else:
+        if amt - cur[x] >= 0:
+            amt -= cur[x]
+        elif amt - cur[x] < 0:
+            r += 1
+            chg.append(cur[x])
+
+        return recur_change(amt, x+r)
